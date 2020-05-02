fc77980b20d92b15f044a5e33c2a59abc4727382
DeusExMachina203 <44448300+DeusExMachina203@users.noreply.github.com>
2020-05-01 21:24:26 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
 #include "funciones.h"
 int main() {
 
-uva146("abaacb");
-uva299();
-for(int i=0; i<6; i++)
-    std::cout<<'s';
+//uva146("abaacb");
+//uva299();
+//uva612();
+uva11340();
 return 0;
 }
